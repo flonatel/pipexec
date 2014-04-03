33fff20a49f97ab0b726b09b6ef4b86b6465fdcd
Andreas Florath <info@flonatel.org>
2014-04-03 17:13:01 +0200

    Fixed ignore return value error/warning

--- test/ptest.c
+++ test/ptest.c
@@ -2,10 +2,10 @@
 
 int main() {
    while(1) {
-      write(1, "ptest out\n", 10);
+      (void)write(1, "ptest out\n", 10);
       char buf[10];
-      read(0, buf, 10);
-      write(3, buf, 10);
+      (void)read(0, buf, 10);
+      (void)write(3, buf, 10);
       sleep(1);
    }
    return 0;
