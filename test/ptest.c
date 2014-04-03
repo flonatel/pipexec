5e9f2e7564d577320cd3c70938d529076fa3937a
Andreas Florath <info@flonatel.org>
2014-04-03 17:18:51 +0200

    Fixed ignore return value error/warning - second try

--- test/ptest.c
+++ test/ptest.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
 int main() {
+   size_t r;
    while(1) {
-      (void)write(1, "ptest out\n", 10);
+      r=write(1, "ptest out\n", 10);
       char buf[10];
-      (void)read(0, buf, 10);
-      (void)write(3, buf, 10);
+      r=read(0, buf, 10);
+      r=write(3, buf, 10);
       sleep(1);
    }
+   (void)r;
    return 0;
 }
