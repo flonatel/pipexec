a83826253d384c26c6f67fc4472f295ed1134f7e
Andreas Florath <info@flonatel.org>
2014-05-13 19:54:11 +0200

    Fixed renamed function

--- src/common_pipe_info.c
+++ src/common_pipe_info.c
@@ -1,4 +1,9 @@
 #include "src/common_pipe_info.h"
+#include "src/logging.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 static void block_fd(pipes_end_info_t const *const pend, int blocking_fd) {
   if (pend->fd > 2 && pend->fd != blocking_fd) {
@@ -13,10 +18,10 @@ static void block_fd(pipes_end_info_t const *const pend, int blocking_fd) {
 // Block all FDs for really used pipes:
 // o open an additional unused pipe
 // o dup2() one fd of this unused pipe for all later on used fds.
-void pipe_info_block_used_fds(pipe_info_t const *const ipipe,
-                              unsigned long const cnt,
-                              parent_pipe_info_t const *const iparent,
-                              size_t const parent_cnt) {
+void common_pipe_info_block_used_fds(pipe_info_t const *const ipipe,
+                                     unsigned long const cnt,
+                                     parent_pipe_info_t const *const iparent,
+                                     size_t const parent_cnt) {
   logging("Blocking used fds");
 
   logging("Creating extra pipe for blocking fds");
