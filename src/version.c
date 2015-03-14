b005a02df1936bdb15998d7a5a606525d12081b4
Andreas Florath <andreas@florath.net>
2015-03-14 17:10:52 +0100

    Fixed copyright info

--- src/version.c
+++ src/version.c
@@ -3,6 +3,7 @@
 int const app_version = 2;
 int const app_subversion = 4;
 
-char const desc_copyight[] = { "(c) 2014 by flonatel GmbH & Co. KG" };
+char const desc_copyight[] = {
+    "(c) 2014-2015 by Andreas Florath <andreas@florath.net"};
 char const desc_license[] = {"License GPLv2+: GNU GPL version 2 or later "
-                             "<http://gnu.org/licenses/gpl.html>." };
+                             "<http://gnu.org/licenses/gpl.html>."};
