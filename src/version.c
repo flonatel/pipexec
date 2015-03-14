e6d9a296d596de2fe446b90d197fb3536e8ad694
Andreas Florath <andreas@florath.net>
2015-03-14 17:34:29 +0100

    First version of ptee man page added

--- src/version.c
+++ src/version.c
@@ -4,6 +4,6 @@ int const app_version = 2;
 int const app_subversion = 4;
 
 char const desc_copyight[] = {
-    "(c) 2014-2015 by Andreas Florath <andreas@florath.net"};
+    "(c) 2014-2015 by Andreas Florath <andreas@florath.net>"};
 char const desc_license[] = {"License GPLv2+: GNU GPL version 2 or later "
                              "<http://gnu.org/licenses/gpl.html>."};
